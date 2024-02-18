4ff8eec27d227b1af756fca39a998074563f6f7d
98yejin <o0o2211915@gmail.com>
2024-02-18 19:20:44 +0900

    UI Challenge - Button

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class App extends StatelessWidget {
               horizontal: 10,
             ),
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 SizedBox(
                   height: 80,
@@ -41,6 +42,51 @@ class App extends StatelessWidget {
                           ),
                         ),
                       ],
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                Text(
+                  'Total Balence',
+                  style: TextStyle(
+                    color: Colors.white.withOpacity(0.8),
+                    fontSize: 22,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  '\$5 194 382',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 42,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                          color: Colors.amber,
+                          borderRadius: BorderRadius.circular(45)),
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(
+                          vertical: 20,
+                          horizontal: 50,
+                        ),
+                        child: Text(
+                          'Transfer',
+                          style: TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
                     )
                   ],
                 )
